7d31c9a0e24e393587446f80cba607a9932d2354
nick <nick2019gnn@gmail.com>
2024-11-10 13:31:55 +0300

    BASH-0005: until

--- bash_app_loop.sh
+++ bash_app_loop.sh
@@ -12,3 +12,15 @@ while (( counter > 0 )); do
 #  counter=$(( counter-1 )) # вариант с присваниванием и использованием Arithmetic expansion
   (( counter-- )) # вариант с декрементом
 done
+echo "================================================="
+
+echo ""
+
+# until - тело выполняется до тех пор, пока условие false
+echo "2. Until"
+counter=10
+until (( counter <= 0 )); do
+  echo "Counter value is : ${counter}"
+#  counter=$(( counter-1 )) # вариант с присваниванием и использованием Arithmetic expansion
+  (( counter-- )) # вариант с декрементом
+done
