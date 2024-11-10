b41ceac98d49bbcb59884099c555b30516ad0969
nick <nick2019gnn@gmail.com>
2024-11-10 13:40:13 +0300

    BASH-0005: Перебор массива с помощью for

--- bash_app_loop.sh
+++ bash_app_loop.sh
@@ -34,3 +34,13 @@ echo "3. For"
 for (( i = 0; i < 5; i++ )); do
   echo "Index value is : ${i}"
 done
+
+# перебор массива
+
+echo ""
+array=(7 92 81 27 63)
+
+echo "Перебор массива с помощью for"
+for (( i = 0; i < "${#array[@]}"; i++ )); do
+  echo "Index value is : ${i}. Array value is : ${array[i]}"
+done
